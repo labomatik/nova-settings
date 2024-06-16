a21e3325ac4b0b23afcea43b152768951f2bf0e0
Christophe <cris@labomatik.com>
2024-06-16 14:04:30 +0200

    Update Namespace

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rdvbiz\NovaSettings;
+namespace Labomatik\NovaSettings;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
--- src/NovaSettings.php
+++ src/NovaSettings.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rdvbiz\NovaSettings;
+namespace Labomatik\NovaSettings;
 
 use Laravel\Nova\Fields\Field;
 
